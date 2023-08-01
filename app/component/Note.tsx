b518ceba221fbb4739370e488a7f4cb2a7e07244
fatihalhijri <fatihalhijri02@gmail.com>
2023-08-01 13:50:11 +0700

    belajar pak rehan

--- app/component/Note.tsx
+++ app/component/Note.tsx
@@ -1,20 +1,21 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 
 type status = "warning" | "error" | "success";
 interface SectionProps {
-    title:string;
-    children:ReactNode;
-    status:status;
+  title: string;
+  children: ReactNode;
+  status: status;
 }
 
-const Note: React.FC<SectionProps> = ({title,status,childen}){
-    return(
-        <section className="border border-red-500 " >
-        <div className="border-b border-red-500">
-        <h5 className="font-bold text-red-500 "></h5></div>
-        <div className="" ></div>
-        </section>
-    )
-}
+const Note: React.FC<SectionProps> = ({ title, status, children }) => {
+  return (
+    <section className=" border border-red-500 mt-5 rounded-lg  shadow-md px-2">
+      <div className="border-b border-red-500  py-2 ">
+        <h5 className="font-bold text-red-500"> {title}</h5>
+      </div>
+      <div className="py-3 text-red-500 text-sm">{children}</div>
+    </section>
+  );
+};
 
-export default Note
+export default Note;
